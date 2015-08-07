3454f114513b226d4d299a11382813256b239e28
gdsanders <gd@digitalequity.com>
2015-08-07 18:50:06 -0500

    Calculator app
    Completed calculator app

---
+++ ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by G.D. Sanders on 2/11/15.
+//  Copyright (c) 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingNumber = false
+    
+    // green arrow from controller to model
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingNumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingNumber = true
+        }
+        
+    }
+   
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingNumber {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+      
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingNumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+        }
+    }
+}
+
